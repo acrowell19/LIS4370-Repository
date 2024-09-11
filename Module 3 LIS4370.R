0d678a39a961ff44a5b80e8e6630acbf81858efb
acrowell19 <156374232+acrowell19@users.noreply.github.com>
2024-09-11 16:16:26 -0400

    Add files via upload

---
+++ Module 3 LIS4370.R
@@ -0,0 +1,17 @@
+#LIS 4370 Module 3 Assignment
+
+#Initial Data
+name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+CBS <- c(12, 75, 43, 19, 1, 21, 19) 
+
+#Creating a data.frame for the data
+poll.results <- data.frame(name, ABC, CBS)
+poll.results
+
+#Plots using the data.frame
+ggplot(poll.results, aes(name, ABC)) +
+  geom_bar(stat = "identity", position = "dodge")
+
+ggplot(poll.results, aes(name, CBS)) +
+  geom_bar(stat = "identity", position = "dodge")
